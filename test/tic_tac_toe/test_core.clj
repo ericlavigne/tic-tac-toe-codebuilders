6a61f3f78880cbfc7c92a889ada7dfeaa8ee9e45
Eric Lavigne <lavigne.eric@gmail.com>
2010-08-29 18:15:47 -0400

    Created test for a new function we are working on.

--- test/tic_tac_toe/test_core.clj
+++ test/tic_tac_toe/test_core.clj
@@ -5,10 +5,11 @@
 
 (def eboard [-1 1 1 0 0 1 -1 1 0])
 
-(deftest test_move_possible 
+(deftest test-move-possible 
   (is (= false (move-possible eboard 7))
       "Move 7 is not possible.")
   (is (= true (move-possible eboard 8))
       "Move 8 is possible."))
       
-		
+(deftest test-possible-moves
+  (is (= [3 4 8] (possible-moves eboard))))
