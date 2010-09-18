fa4de88e9f326a75b7b81f5ba06f30a48bcc42a6
Algarete13 <joelmontero13@gmail.com>
2010-09-17 20:42:03 -0400

    Added clj-stacktrace 0.2.0 to our dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject tic-tac-toe "1.0.0-SNAPSHOT"
   :description "A tic-tac-toe game"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+                   [org.clojure/clojure-contrib "1.2.0"]
+		   [clj-stacktrace "0.2.0"]])
