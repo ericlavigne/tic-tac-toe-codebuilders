ac67f5a9307b62a190026b65becd99c3340506d2
Eric Lavigne <lavigne.eric@gmail.com>
2010-09-19 17:20:46 -0400

    More formatting improvements

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -3,32 +3,32 @@
 (def all-moves (range 9))
 
 (defn is-move-possible [board move]
-    (= 0 (nth board move))) 
+  (= 0 (nth board move))) 
 
 (defn possible-moves [board]
-    (filter (fn [move]
-                (is-move-possible board move))
-            all-moves))
+  (filter (fn [move]
+	    (is-move-possible board move))
+	  all-moves))
 
 (defn what-board-looks-like-after-move [board move player]
-    (assoc board move player))
+  (assoc board move player))
     
 (defn is-board-full [board]
-    (empty? (possible-moves board)))
+  (empty? (possible-moves board)))
     
 (def winning-combinations 
-    [[0 1 2][3 4 5] 
-    [6 7 8][0 3 6] 
-    [1 4 7][2 5 8] 
-    [0 4 8][2 4 6]])
+     [[0 1 2] [3 4 5] 
+      [6 7 8] [0 3 6] 
+      [1 4 7] [2 5 8] 
+      [0 4 8] [2 4 6]])
     
 (defn won-by-method? [board method player]
-    (every? (fn [position]
-		    (= player
-			(nth board position)))
-		method))
+  (every? (fn [position]
+	    (= player
+	       (nth board position)))
+	  method))
 		
 (defn player-won? [board player]
-    (some (fn [winning-combination]
-              (won-by-method? board winning-combination player))
-          winning-combinations))
+  (some (fn [winning-combination]
+	  (won-by-method? board winning-combination player))
+	winning-combinations))
